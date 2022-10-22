8c0f5ac67d077c21ef7b0ae15fe26ade352e7ffe
fikrianggara <fikriseptriananggara@gmail.com>
2022-10-23 06:24:38 +0700

    adding kehamilan page

--- src/components/pages/Kehamilan.js
+++ src/components/pages/Kehamilan.js
@@ -5,7 +5,13 @@ import {
   AccountCircle,
   MonitorHeart,
   PregnantWomanOutlined,
+  BabyChangingStation,
+  Favorite,
+  ChildCare,
+  ArrowBackOutlined,
 } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
 import moment from "moment-with-locales-es6";
 moment.locale("id");
 
@@ -14,10 +20,12 @@ const Kehamilan = ({ data }) => {
   const user = data[0] ? data[0] : null;
   const [pregnancyData, setPregnancyData] = useState(null);
   let pregnancyAge = 0;
-  let pemeriksaanTerakhir = 0;
+  let lastCheckup = 0;
+  let jarakHinggaKelahiran = 0;
+
   useEffect(() => {
     getPregnancyDataByUserId(id, setPregnancyData);
-  }, []);
+  }, [id]);
 
   if (pregnancyData !== null) {
     pregnancyAge = moment(
@@ -30,9 +38,15 @@ const Kehamilan = ({ data }) => {
       .split(" ")
       .splice(0, 2)
       .join(" ");
-    pemeriksaanTerakhir = moment(
+    lastCheckup = moment(
       pregnancyData[0]?.pemeriksaan_terakhir
         ? pregnancyData[0]?.pemeriksaan_terakhir.tanggal_pemeriksaan
+        : new Date()
+    ).fromNow();
+
+    jarakHinggaKelahiran = moment(
+      pregnancyData[0]?.estimasi_tanggal_kelahiran
+        ? pregnancyData[0]?.estimasi_tanggal_kelahiran
         : new Date()
     ).fromNow();
   }
@@ -42,23 +56,57 @@ const Kehamilan = ({ data }) => {
       {pregnancyData ? (
         <div>
           {/* patient overview */}
-          <header className="bg-sky-500 p-4 text-white ">
-            <nav className="flex items-center justify-between text-sm">
-              <div className="flex space-x-2 items-center ">
-                <AccountCircle fontSize="large"></AccountCircle>
-                <div>
-                  <h1 className="font-medium">{user.nama}</h1>
-                  <h2>
-                    <PregnantWomanOutlined></PregnantWomanOutlined>
-                    {pregnancyAge}
-                  </h2>
+          <header className="bg-white pb-10 h-[350px] relative">
+            <div className="bg-sky-500 p-4 text-white space-y-2 rounded-b-2xl">
+              <nav className="flex items-center justify-between text-sm">
+                <div className="flex space-x-2 items-center ">
+                  <AccountCircle fontSize="large"></AccountCircle>
+                  <div>
+                    <h1 className="font-medium">{user.nama}</h1>
+                    <h2>
+                      <PregnantWomanOutlined></PregnantWomanOutlined>
+                      {pregnancyAge}
+                    </h2>
+                  </div>
                 </div>
+                <div className="flex items-center space-x-2 ">
+                  <MonitorHeart></MonitorHeart>
+                  <h1>{lastCheckup}</h1>
+                </div>
+              </nav>
+              <hr />
+              <ul className="p-4 space-y-2 text-sm">
+                <li>
+                  <BabyChangingStation></BabyChangingStation>
+                  {jarakHinggaKelahiran}
+                </li>
+                <li className="flex space-x-4">
+                  <div className="flex items-center">
+                    <PregnantWomanOutlined></PregnantWomanOutlined>
+                    <div className="relative">
+                      <Favorite fontSize="large"></Favorite>
+                      <span className="absolute z-10 text-red-400 left-5 top-1 font-bold text-xs">
+                        +
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex items-center">
+                    <ChildCare></ChildCare>
+                    <div className="relative">
+                      <Favorite fontSize="large"></Favorite>
+                      <span className="absolute z-10 text-red-400 left-5 top-1 font-bold text-xs">
+                        +
+                      </span>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <Link to="/">
+              <div className="w-12 h-12 p-2 absolute bottom-4 left-4 rounded bg-gray-200 flex items-center text-sky-700">
+                <ArrowBackOutlined fontSize="large"></ArrowBackOutlined>
               </div>
-              <div className="flex items-center space-x-2 ">
-                <MonitorHeart></MonitorHeart>
-                <h1>{pemeriksaanTerakhir}</h1>
-              </div>
-            </nav>
+            </Link>
           </header>
           {/* patient overview end */}
           <main></main>
