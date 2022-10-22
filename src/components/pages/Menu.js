ff1565377f96bab5b33aa9ace16860e466cf1f6e
fikrianggara <fikriseptriananggara@gmail.com>
2022-10-23 03:05:43 +0700

    update firebase configuration

--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div>
+      <header className="text-center p-4">
+        <h1 className="text-black text-2xl font-bold">Monitoring Kehamilan</h1>
+      </header>
+      <main></main>
+      <footer className="text-center underline text-black">
+        <p>Berikan Saran</p>
+      </footer>
+    </div>
+  );
 };
 
 export default Menu;
