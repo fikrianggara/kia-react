546e437355f7e85f9c03d07ef654cf66a19306f3
fikrianggara <fikriseptriananggara@gmail.com>
2022-11-02 12:30:12 +0700

    ketinggalan waokaowk

--- src/pages/ibu-anak/kehamilan/Detail.js
+++ src/pages/ibu-anak/kehamilan/Detail.js
@@ -258,9 +258,6 @@ export const Detail = ({ data }) => {
             </PemeriksaanCard>
             {/* timbangan end */}
             {/* usg */}
-            <PemeriksaanCard title={'USG'} info={}>
-
-            </PemeriksaanCard>
             <div className="border rounded-lg p-4 space-y-2">
               <div className="flex text-sky-800 font-medium items-center justify-between">
                 <h3 id="timbang">USG</h3>
